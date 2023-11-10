7a2504ba0bdfeafb4f0e1a06493641ec6083b887
Vanessa Ponte <vanessaponte@gmail.com>
2023-11-10 12:37:32 -0300

    style: login

--- burguer-queen/src/app/components/login/login.module.ts
+++ burguer-queen/src/app/components/login/login.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { FormLoginComponent } from './form-login/form-login.component';
+import { TopIconComponent } from './top-icon/top-icon.component';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    FormLoginComponent
+    FormLoginComponent,
+    TopIconComponent
     // chamar component commons aqui
   ],
   imports: [
