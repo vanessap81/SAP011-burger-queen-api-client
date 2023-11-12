fb58f7e3accd928c4e327c343a4c717c3dbf3516
Vanessa Ponte <vanessaponte@gmail.com>
2023-11-12 09:22:23 -0300

    feat: service config

--- burguer-queen/src/app/services/login.service.ts
+++ burguer-queen/src/app/services/login.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Login } from '../Login';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class LoginService {
+  private readonly apiUrl = 'https://burger-queen-api-jade.vercel.app/login'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  
+  login(formData: FormData): Observable<FormData> {
+    return this.http.post<FormData>(this.apiUrl, formData);
+  }
 }
