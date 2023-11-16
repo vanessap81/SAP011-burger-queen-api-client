ec219efdc5472c671833d51abdd3ee31acb2f907
Vanessa Ponte <vanessaponte@gmail.com>
2023-11-16 13:24:53 -0300

    test: teste do login.service

--- burguer-queen/src/app/services/login.service.spec.ts
+++ burguer-queen/src/app/services/login.service.spec.ts
@@ -1,16 +1,32 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Login } from '../interfaces/Login';
 
 import { LoginService } from './login.service';
+import { HttpClient } from '@angular/common/http';
 
-describe('LoginService', () => {
+fdescribe('LoginService', () => {
   let service: LoginService;
+  let http: HttpClient;
+  let form: Login
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
     service = TestBed.inject(LoginService);
+    http = TestBed.inject(HttpClient)
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('deve chamar um POST com o endpoint correto', () => {
+    const spy = spyOn(http, 'post').and.callThrough();
+    service.login(form);
+    expect(spy).toHaveBeenCalled()
+  })
 });
