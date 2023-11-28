74952d92f1cbac0a84ff7b24978836cbf2038e24
Vanessa Ponte <vanessaponte@gmail.com>
2023-11-28 13:50:18 -0300

    comentários removidos

--- burguer-queen/src/app/components/waiter/menu/menu.component.ts
+++ burguer-queen/src/app/components/waiter/menu/menu.component.ts
@@ -61,41 +61,33 @@ export class MenuComponent implements OnInit {
 
   removeProduct(event: Event, product: ProductResponse) {
     const target = event.target as HTMLInputElement;
-
     product.quantity--;
     if(this.order.products.length !== 0) {
       this.order.products.forEach(item => {
         if(item.productId === target.value && item.quantity > 0){
           item.quantity--;
-          console.log('quantidade diminuida');
         } 
       })
     }
 
     let filtredProducts = this.order.products.filter(((item) => item.quantity > 0));
     this.order.products = filtredProducts;
-    console.log(this.order.products);
   };
 
   itensInThisOrder = new Set();
 
   addProduct(event: Event, product: ProductResponse) {
     const target = event.target as HTMLInputElement;
-    console.log(target.value);
 
-    console.log(this.itensInThisOrder);
-    
     product.quantity++;
 
     if (this.order.products.length === 0) {
       this.order.products.push({productId: target.value, quantity: 1});
       this.itensInThisOrder.add(target.value);
-    }
-    else if (this.order.products.length > 0 && this.itensInThisOrder.has(target.value) === false) {
+    } else if (this.order.products.length > 0 && this.itensInThisOrder.has(target.value) === false) {
       this.order.products.push({productId: target.value, quantity: 1});
       this.itensInThisOrder.add(target.value);
-    } 
-    else if (this.order.products.length > 0 && this.itensInThisOrder.has(target.value) === true) {
+    } else if (this.order.products.length > 0 && this.itensInThisOrder.has(target.value) === true) {
       for (let i = 0; i < this.order.products.length; i++) {
         if (target.value === this.order.products[i].productId) {
           this.order.products[i].quantity++;
@@ -107,26 +99,3 @@ export class MenuComponent implements OnInit {
   }
 
 }
-
-// if(this.order.products.length > 0) {
-//   for (let i = 0; i < this.order.products.length; i++) {
-//     if (target.value !== this.order.products[i].productId) {
-//       let productInclueded: Products = {productId: target.value, quantity: 0};
-//       productInclueded.quantity++;
-//       this.order.products.push(productInclueded);
-//       console.log('novo produto incluido no pedido');
-//     } 
-    
-//     if (target.value === this.order.products[i].productId) {
-//       this.order.products[i].quantity++;
-//       console.log('quantidade acrescida');
-//     }
-//   };
-// } 
-
-// else if(this.order.products.length === 0) {
-//   let productInclueded: Products = {productId: target.value, quantity: 0};
-//   productInclueded.quantity++;
-//   this.order.products.push(productInclueded);
-//   console.log('novo produto incluido no pedido');
-// } 
